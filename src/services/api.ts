7829b957b996b7d5ecaaadccc85f312b7726e521
jamescasia <jamescasia99@gmail.com>
2025-04-10 16:58:59 +0800

    Update API_URL

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,10 @@
 import axios from "axios";
 
-export const API_URL = "https://url-shortener-service-833302674742.asia-southeast1.run.app";
+if (!process.env.NEXT_PUBLIC_API_BASE_URL) {
+  throw new Error("Missing NEXT_PUBLIC_API_BASE_URL in environment variables");
+}
+
+export const API_URL = 'https://urlly.jamescasia.com';
 export const shortenUrl = async (originalUrl: string): Promise<any> => {
   try {
     const response = await axios.post(`${API_URL}/urls/shorten`, {
